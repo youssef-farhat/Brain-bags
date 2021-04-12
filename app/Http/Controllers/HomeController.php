34d84a97722ccebc9b606236e1e8d10488d6eb36
youssef-farhat <youssef.farhat@bizerte.r-iset.tn>
2021-04-12 19:44:52 +0100

    second-try

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,10 +8,10 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function welcome(){
-        $id=0;
-        $client=Client::all();
-        var_dump($client);
-        return view('welcome');
+    
+        
+        
+        return view('welcome' , [ 'clients'=>Client::all() ]);
     }
     
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,16 +84,13 @@
                     ISETB - DSI21 - 2021
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <ul>
+                @foreach($clients as $client)
+                    <li> {{ $client['nom']}} ( {{ $client['tel']}} ) {{ $client['adresse']}}   </li>
+                @endforeach
+                            
+                
+                </ul>
             </div>
         </div>
     </body>
