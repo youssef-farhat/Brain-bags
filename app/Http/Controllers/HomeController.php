9f9aaeebe6e4d89cb38a3ea3f245f71c180c3ef1
youssef-farhat <youssef.farhat@bizerte.r-iset.tn>
2021-04-14 01:18:04 +0100

    third-commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,16 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use App\Client;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function welcome(){
-    
-        
-        
-        return view('welcome' , [ 'clients'=>Client::all() ]);
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
-    
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
 }
