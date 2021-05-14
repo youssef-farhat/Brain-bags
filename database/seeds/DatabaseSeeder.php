eec08a9b334a3dd68f8143b1c547347680e58c03
rayen-naffouti <naffoutirayen11@gmail.com>
2021-05-14 15:12:07 +0200

    test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,21 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-<<<<<<< HEAD
         $this->call(EntreprisesTableSeeder::class);
         $this->call(StagesTableSeeder::class);
-=======
-        // youssef
-        $this->call(EnseignantTableSeeder::class);
-        $this->call(SoutenanceTableSeeder::class);  
-        $this->call(AdministrateurTableSeeder::class);
-
-        
-        $this->call(EtudiantTableSeeder::class);
-         $this->call(EntrepriseTableSeeder::class);
-         $this->call(StageTableSeeder::class);
-         $this->call(DemandeTableSeeder::class);
-
->>>>>>> f2b3ec73dd71f6433191b10190669f3c0e6f6aa1
     }
 }
